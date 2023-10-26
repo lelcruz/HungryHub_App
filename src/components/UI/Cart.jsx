ef375382efe585287a247dd6303b656ee8354535
Leslie Cruz <lesliecruz2@my.unt.edu>
2023-10-26 12:42:02 -0500

    working on the cart items. Where you can remove and add items from the cart

--- src/components/UI/Cart.jsx
+++ src/components/UI/Cart.jsx
@@ -4,6 +4,7 @@ import CartContext from "../../store/CartContext.jsx";
 import {currencyFormatter} from "../../util/formatting.js";
 import Button from "./Button.jsx";
 import userProgressContext from "../../store/UserProgressContext.jsx";
+import CartItem from "./CartItem.jsx";
 
 export default function Cart(){
    const cartCtx = useContext(CartContext);
@@ -19,7 +20,9 @@ export default function Cart(){
         <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
             <h2>Your Cart</h2>
             <ul>
-                {cartCtx.items.map(item => <li key={item}>{item.name} - {item.quantity}</li>)}
+                {cartCtx.items.map(item =>
+                <CartItem key={item} name={item.name} quantity={item.quantity} price={item.price}
+                onIncrease={() => cartCtx.addItem(item)} onDecrease={() => cartCtx.removeItem(item.id)}/>)}
             </ul>
             <p className="cart-total"> {currencyFormatter.format(cartTotal)}</p>
             <p className="modal-actions">
